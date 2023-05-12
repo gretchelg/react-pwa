9c947c37c383208d7d484570ef4d3cdcae94810c
gretchelg <gretchelglopez@gmail.com>
2023-05-12 19:50:21 +0200

    install button added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './App.css';
 // import Camera from './components/Camera';
-import WebcamCapture from './components/WebcamCapture'
+import WebcamCapture from './components/WebcamCapture';
+import { usePWAInstall } from 'react-use-pwa-install'
 
 function App() {
+  const install = usePWAInstall()
   return (
     <div className="App">
+      <header>
+			{<button onClick={install}>Install App</button>}
+		</header>
       <WebcamCapture />
       {/* <Camera /> */}
     </div>
